e9d38c767e5b0d18fc4ad7b05ff1c0827a3c62d9
Yang Sheng Han <progamesigner@gmail.com>
2019-05-04 14:16:03 +0800

    Refine not found page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import burger from './scripts/burger'
 import collapse from './scripts/collapse'
+import emoji from './scripts/emoji'
 import highlight from './scripts/highlight'
 import open from './scripts/open'
 import progress from './scripts/progress'
@@ -11,6 +12,7 @@ const bootstrap = () => {
     document.querySelectorAll('.navbar-burger').forEach(burger)
     document.querySelectorAll('.navbar.is-fixed-top').forEach(item => collapse(item, 60))
     document.querySelectorAll('.page-share.is-fixed-left').forEach(item => collapse(item, getElementTop(document.querySelectorAll('.page-body')) - 60))
+    document.querySelectorAll('body').forEach(emoji)
     document.querySelectorAll('.chroma > table').forEach(highlight)
     document.querySelectorAll(`a[rel~="external"], a[href^="http"]:not([href^="${location.protocol}//${location.host}"])`).forEach(open)
     document.querySelectorAll('.progress').forEach(item => progress(item, 0))
---
+++ src/scripts/emoji.js
@@ -0,0 +1,12 @@
+const emojis = [
+    '\\(o_o)/', '(o^^)o', '(˚Δ˚)b', '(^-^*)',
+    '(≥o≤)', '(^_^)b', '(·_·)', '(=\'X\'=)',
+    '(>_<)', '(;-;)', '\\(^Д^)/'
+]
+
+export default container => {
+    container.querySelectorAll('[data-emoji]').forEach(function (element) {
+        var emoji = emojis[Math.floor(Math.random() * emojis.length)]
+        element.appendChild(document.createTextNode(emoji))
+    })
+}
