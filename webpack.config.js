ad5795adea89b68701269a70c845fa1173ca8116
Yang Sheng Han <progamesigner@gmail.com>
2021-04-21 17:34:56 +0800

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,75 +3,77 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, options) => {
-    const isProd = options.mode === 'production'
+  const isProd = options.mode === 'production'
 
-    return {
-        entry: {
-            'main': ['./src/main.js', './src/main.scss'],
-            'amp': ['./src/amp.scss']
+  return {
+    entry: {
+      'main': ['./src/main.js', './src/main.scss'],
+      'amp': ['./src/amp.scss']
+    },
+    output: {
+      path: path.join(__dirname, 'assets'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         },
-        output: {
-            path: path.join(__dirname, 'assets'),
-            filename: '[name].js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.s[ac]ss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
+        {
+          test: /\.s[ac]ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: !isProd,
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: !isProd,
+                postcssOptions: {
+                  plugins: [
+                    require('postcss-inline-svg')({
+                      removeFill: true,
+                      xmlns: false
+                    }),
+                    require('cssnano')({
+                      preset: [
+                        'default',
                         {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: !isProd,
-                                importLoaders: 1
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: !isProd,
-                                plugins: [
-                                    require('postcss-inline-svg')({
-                                        removeFill: true,
-                                        xmlns: false
-                                    }),
-                                    require('cssnano')({
-                                        preset: [
-                                            'default',
-                                            {
-                                                autoprefixer: {
-                                                },
-                                                discardComments: {
-                                                    removeAll: !isProd
-                                                }
-                                            }
-                                        ]
-                                    })
-                                ]
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: !isProd
-                            }
+                          autoprefixer: {
+                          },
+                          discardComments: {
+                            removeAll: !isProd
+                          }
                         }
-                    ]
+                      ]
+                    })
+                  ]
                 }
-            ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: '[name].css'
-            })
-        ]
-    }
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: !isProd
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      })
+    ]
+  }
 }
